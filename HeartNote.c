c4ae7c3d0094d7b4643b076c7371ebd5ddf27154
Yehezkiel Jonatan <ezekielyonathan@gmail.com>
2021-10-11 15:49:45 +0700

    Add user input functionality: user can now input a text that is saved in a file

--- HeartNote.c
+++ HeartNote.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
+#include <direct.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    // For storing text.
+    char content[1024];
+
+    // User inputs the text.
+    // TODO: prone to buffer overflow.
+    printf("Content:\n");
+    scanf("%s", content);
+    
+    // Save the text in a file within a subdirectory.
+    _mkdir(".\\Entries");
+    FILE *fptr = fopen(".\\Entries\\entry.txt", "w");
+    if (fptr == NULL) {
+        printf("AN ERROR OCCURRED!\n");
+        _getch();
+        exit(1);
+    }
+    fprintf(fptr, "%s", content);
+    fclose(fptr);
+
     _getch();
-    return 0;
+    exit(0);
 }
